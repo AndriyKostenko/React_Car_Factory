3b6cbd7cca83f26984de24e2d66c79f02042ade0
Andriy Kostenko <a.kostenkouk@gmail.com>
2023-09-19 12:03:37 -0400

    fixed few bugs of form in join sect.

---
+++ src/components/appFooter/AppFooter.jsx
@@ -0,0 +1,22 @@
+import './appFooter.scss';
+
+
+
+
+const AppFooter = () => {
+
+
+    return (
+        <footer className="app__footer dark__bg">
+            <div className="container">
+                <div className="app__footer-wrapper">
+                    
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default AppFooter;
+
+
--- src/components/appJoin/AppJoin.jsx
+++ src/components/appJoin/AppJoin.jsx
@@ -118,6 +118,7 @@ const AppJoin = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        setValues({buttonText:'Sending...'});
 
         if (!values.uploadResume || !values.isChecked) {
             setErrorMessage({...errorMessage, uploadResume:'Please Upload CV'});
@@ -184,7 +185,6 @@ const AppJoin = () => {
                             </div>
                         </div>
   
-                       
 
                         <div className='app__join-form-input-last'>
                             <input type="lastName"
@@ -202,7 +202,6 @@ const AppJoin = () => {
                             </div>
                         </div>
 
-                        
 
                         <div className='app__join-form-input-email'>
                             <input type="email" 
@@ -219,9 +218,6 @@ const AppJoin = () => {
                                 <span>{errorMessage.email}</span>
                             </div>
                         </div>
-
-                        
-
 
 
                         <div className='app__join-form-input-phone'>
@@ -245,58 +241,56 @@ const AppJoin = () => {
                         </div>
 
                        
+                        <div className="app__join-form-input-message">
+                            <label className="app__join-form-input-message-label">
+                                Message
+                            </label>
+                            <textarea name="message" 
+                                    cols="30" 
+                                    rows="5" 
+                                    className="input"
+                                    onChange={onChange}
+                                    onBlur={handleFocus}
+                                    focused={focused.toString()}
+                                    spellCheck="false">      
+                            </textarea>
+                        </div>
 
-                        
-                        <label className="app__join-form-input-message-label">
-                            Message
-                        </label>
-                        <textarea name="message" 
-                                cols="30" 
-                                rows="2" 
-                                className="app__join-form-input-message input"
-                                onChange={onChange}
-                                onBlur={handleFocus}
-                                focused={focused.toString()}
-                                spellCheck="false">
-                                    
-                        </textarea>
-                        
 
-                        
+
+                        <div className="app__join-form-resume">
+                            <input type="file" 
+                                    name='uploadResume' 
+                                    id='uploadResume' 
+                                    accept=".pdf,.docx,.doc" 
+                                    onChange={handleFileChange} 
+                                    required={true}
+                                    className='app__join-form-resume-hide'
+                                    onBlur={handleFocus}
+                                />
+                            <label htmlFor="uploadResume" 
+                                    className="app__join-form-resume-show"
+                                    >
+                                <span className="app__join-form-resume-icon">
+                                    <svg width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5.5 20C3.96667 20 2.66667 19.4667 1.6 18.4C0.533333 17.3333 0 16.0333 0 14.5V4C0 2.9 0.391667 1.95833 1.175 1.175C1.95833 0.391667 2.9 0 4 0C5.1 0 6.04167 0.391667 6.825 1.175C7.60833 1.95833 8 2.9 8 4V13.5C8 14.2 7.75833 14.7917 7.275 15.275C6.79167 15.7583 6.2 16 5.5 16C4.8 16 4.20833 15.7583 3.725 15.275C3.24167 14.7917 3 14.2 3 13.5V4H4.5V13.5C4.5 13.7833 4.596 14.021 4.788 14.213C4.98 14.405 5.21733 14.5007 5.5 14.5C5.78333 14.5 6.021 14.404 6.213 14.212C6.405 14.02 6.50067 13.7827 6.5 13.5V4C6.5 3.3 6.25833 2.70833 5.775 2.225C5.29167 1.74167 4.7 1.5 4 1.5C3.3 1.5 2.70833 1.74167 2.225 2.225C1.74167 2.70833 1.5 3.3 1.5 4V14.5C1.5 15.6 1.89167 16.5417 2.675 17.325C3.45833 18.1083 4.4 18.5 5.5 18.5C6.6 18.5 7.54167 18.1083 8.325 17.325C9.10833 16.5417 9.5 15.6 9.5 14.5V4H11V14.5C11 16.0333 10.4667 17.3333 9.4 18.4C8.33333 19.4667 7.03333 20 5.5 20Z" fill="#F2F6F8" fillOpacity="0.8"/>
+                                    </svg>
+                                </span>
+
+                                {values.uploadResume ? (
+                                    <span className='app__join-form-resume-text'>{values.uploadResume.name}</span>
+                                    ) : (
+                                    <span className="app__join-form-resume-text">Upload a Resume</span>
+                                    )
+                                }
+
+                                {errorMessage.uploadResume && <div className='app__join-form-resume-error'>{errorMessage.uploadResume}</div>}
+                            </label>
+                        </div>
 
                        
-                        <input type="file" 
-                                name='uploadResume' 
-                                id='uploadResume' 
-                                accept=".pdf,.docx,.doc" 
-                                onChange={handleFileChange} 
-                                required={true}
-                                className='app__join-form-resume-hide'
-                                onBlur={handleFocus}
-                            />
-                        <label htmlFor="uploadResume" 
-                                className="app__join-form-resume-show"
-                                >
-                            <span className="app__join-form-resume-icon">
-                                <svg width="11" height="20" viewBox="0 0 11 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M5.5 20C3.96667 20 2.66667 19.4667 1.6 18.4C0.533333 17.3333 0 16.0333 0 14.5V4C0 2.9 0.391667 1.95833 1.175 1.175C1.95833 0.391667 2.9 0 4 0C5.1 0 6.04167 0.391667 6.825 1.175C7.60833 1.95833 8 2.9 8 4V13.5C8 14.2 7.75833 14.7917 7.275 15.275C6.79167 15.7583 6.2 16 5.5 16C4.8 16 4.20833 15.7583 3.725 15.275C3.24167 14.7917 3 14.2 3 13.5V4H4.5V13.5C4.5 13.7833 4.596 14.021 4.788 14.213C4.98 14.405 5.21733 14.5007 5.5 14.5C5.78333 14.5 6.021 14.404 6.213 14.212C6.405 14.02 6.50067 13.7827 6.5 13.5V4C6.5 3.3 6.25833 2.70833 5.775 2.225C5.29167 1.74167 4.7 1.5 4 1.5C3.3 1.5 2.70833 1.74167 2.225 2.225C1.74167 2.70833 1.5 3.3 1.5 4V14.5C1.5 15.6 1.89167 16.5417 2.675 17.325C3.45833 18.1083 4.4 18.5 5.5 18.5C6.6 18.5 7.54167 18.1083 8.325 17.325C9.10833 16.5417 9.5 15.6 9.5 14.5V4H11V14.5C11 16.0333 10.4667 17.3333 9.4 18.4C8.33333 19.4667 7.03333 20 5.5 20Z" fill="#F2F6F8" fillOpacity="0.8"/>
-                                </svg>
-                            </span>
 
-                            {values.uploadResume ? (
-                                <span className='app__join-form-resume-text'>{values.uploadResume.name}</span>
-                                ) : (
-                                <span className="app__join-form-resume-text">Upload a Resume</span>
-                                )
-                            }
-
-                            {errorMessage.uploadResume && <div className='app__join-form-resume-error'>{errorMessage.uploadResume}</div>}
-                        </label>
                        
-
-                        
-
-
 
                         <div className="app__join-form-input-terms">
                             <label className='custom-checkbox'>
